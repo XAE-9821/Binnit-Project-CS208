179b73116a76e98be643eebd0569b55ba4a90370
XAE-9821 <ggopinthehouse@gmail.com>
2025-03-16 03:45:54 +0530

    test pull

---
+++ Bnnit/login_page.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const images = [
+        "image_scrolls/deviant5.gif",
+        "image_scrolls/deviant7.png",
+        "image_scrolls/deviant8.gif",
+        "image_scrolls/deviant9.gif",
+        "image_scrolls/deviant10.gif",
+        "image_scrolls/deviant11.gif",
+        "image_scrolls/deviant12.gif",
+        "image_scrolls/deviant13.gif"
+  
+    ];
+
+    let lastIndex = -1;
+
+    function getRandomIndex() {
+        let newIndex;
+        do {
+            newIndex = Math.floor(Math.random() * images.length);
+        } while (newIndex === lastIndex); 
+        lastIndex = newIndex;
+        return newIndex;
+    }
+
+    const randomImage = images[getRandomIndex()];
+
+    const bgElement = document.getElementById("bgImage");
+    bgElement.style.backgroundImage = `url(${randomImage})`;
+    bgElement.style.backgroundSize = "cover";      
+    bgElement.style.backgroundPosition = "center"; 
+    bgElement.style.backgroundRepeat = "no-repeat"; 
+});
